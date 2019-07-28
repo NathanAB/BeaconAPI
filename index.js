2760d569d78301aaed5636e94c5cfc244c731d5d
Nathan Bakirci <nathan@virtru.com>
2019-07-28 11:17:37 -0400

    USe server

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ mountRoutes(app);
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
-app.listen(process.env.PORT || 8080, () => {
+const server = app.listen(process.env.PORT || 8080, () => {
   const port = server.address().port;
   console.log("App now running on port", port);
 });
