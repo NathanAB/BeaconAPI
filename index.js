ad03dfe702b3dfd37f7be844d8a722d703ec06f5
Nathan Bakirci <nathan@virtru.com>
2019-07-28 11:12:25 -0400

    Change port

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const express = require('express');
 
 const mountRoutes = require('./routes');
 
-const port = 3000;
 const app = express();
 mountRoutes(app);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
 app.get('/', (req, res) => res.send('Hello world!'));
+
+app.listen(process.env.PORT || 8080, () => {
+  const port = server.address().port;
+  console.log("App now running on port", port);
+});
