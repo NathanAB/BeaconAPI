c3a50c8c810cb5d78e159c442105482645dfc305
Nathan Bakirci <nathan@virtru.com>
2019-08-17 18:40:46 -0400

    Add CORS header

--- index.js
+++ index.js
@@ -9,12 +9,21 @@ const initRoutes = require('./routes');
 const app = express();
 
 initAuth(passport);
+
 app.use(cookieSession({
   name: 'session',
   keys: ['123'],
 }));
+
 app.use(cookieParser());
+
 app.use(passport.initialize());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'app.beacondates.com');
+  next();
+});
+
 initRoutes(app);
 
 const port = process.env.PORT || 8080;
