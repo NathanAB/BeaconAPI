99ac6ebb3994af355cf9af4155cf364d1f300e25
Nathan Bakirci <nathan@virtru.com>
2020-02-19 11:20:19 -0500

    Update CORS and app URL

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 module.exports = {
   APP_URL: process.env.NODE_ENV === 'dev'
     ? 'https://local.beacondates.com'
-    : 'https://app.beacondates.com',
+    : 'https://www.beacondates.com',
   API_URL: process.env.NODE_ENV === 'dev'
     ? 'https://local.beacondates.com'
-    : 'https://api.beacondates.com',
+    : 'https://www.beacondates.com',
 };
