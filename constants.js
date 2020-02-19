e8a0e22abaa24f70ad47d000c98b54d3390ff347
Nathan Bakirci <nathan@virtru.com>
2020-02-19 12:34:38 -0500

    Fix URLs

--- constants.js
+++ constants.js
@@ -4,5 +4,5 @@ module.exports = {
     : 'https://www.beacondates.com',
   API_URL: process.env.NODE_ENV === 'dev'
     ? 'https://local.beacondates.com'
-    : 'https://www.beacondates.com',
+    : 'https://api.beacondates.com',
 };
